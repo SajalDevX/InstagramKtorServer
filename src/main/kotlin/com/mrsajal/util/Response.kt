b37217351004c1a40fa36cffd2d1cb2c3ebcbce1
Sajal <kakalijana1254@gmail.com>
2024-06-11 20:31:07 +0530

    Initial Commit

--- src/main/kotlin/com/mrsajal/util/Response.kt
+++ src/main/kotlin/com/mrsajal/util/Response.kt
@@ -9,3 +9,6 @@ sealed class Response<T>(
     class Success<T>(data: T) : Response<T>(data = data)
     class Error<T>(code: HttpStatusCode, data: T) : Response<T>(code = code, data = data)
 }
+
+
+///
