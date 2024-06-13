6c1a6d41bc76eb89118ef6eb97a01b5be5e81f5f
Sajal <kakalijana1254@gmail.com>
2024-06-13 20:45:45 +0530

    Post Comments Implementation done

--- src/main/kotlin/com/mrsajal/plugins/Routing.kt
+++ src/main/kotlin/com/mrsajal/plugins/Routing.kt
@@ -5,7 +5,6 @@ import io.ktor.server.application.*
 import io.ktor.server.http.content.*
 import io.ktor.server.routing.*
 
-
 fun Application.configureRouting() {
     routing {
         authRouting()
